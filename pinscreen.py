b6bfddc5614e897e702017177b4c5263015e7439
Tim D. Smith <git@tim-smith.us>
2015-04-04 15:18:45 -0700

    pep8

--- pinscreen.py
+++ pinscreen.py
@@ -31,7 +31,7 @@ class Dot:
 
 class SineFit:
     "Stores parameters for an arbitrary sine function."
-    def __init__(self, amplitude, period, phase, offset, r2 = None):
+    def __init__(self, amplitude, period, phase, offset, r2=None):
         self.amplitude = amplitude
         self.period = period
         self.phase = phase
@@ -51,10 +51,10 @@ class SineFit:
     def eval(self, t):
         "SineFit.eval(self,t): Evaluate the sine function represented by self at a point or list of points t."
         singleton = not getattr(t, '__iter__', False)
-        if singleton: t = [t]
+        if singleton:
+            t = [t]
         ret = [self.amplitude * sawtooth(2*pi/self.period * ti + self.phase, width=0.5) + self.offset for ti in t]
-        if singleton: return ret[0]
-        return ret
+        return ret[0] if singleton else ret
 
     def __repr__(self):
         return ('SineFit(amplitude=%f, period=%f, phase=%f, offset=%f, r2=%r)' %
@@ -75,7 +75,7 @@ def parse_mtrack2(fileobj):
     # x_col[1] is the index in line for X1
     x_col, y_col = [1 + i*3 for i in xrange(n)], [2 + i*3 for i in xrange(n)]
     assignments = None
-    fileobj.readline() # discard the line "Tracks 1 to n"
+    fileobj.readline()  # discard the line "Tracks 1 to n"
     frames = []
     for line in fileobj.readlines():
         line = line[:-1].split('\t')
@@ -87,8 +87,8 @@ def parse_mtrack2(fileobj):
             # is in the first 2n, it must be in the second column, and so on.
             x, y = ([(i, float(line[col])) for i, col in enumerate(x_col)],
                     [(i, float(line[col])) for i, col in enumerate(y_col)])
-            x = sorted(x, cmp=lambda a,b: cmp(a[1], b[1]))
-            y = sorted(y, cmp=lambda a,b: cmp(a[1], b[1]))
+            x = sorted(x, cmp=lambda a, b: cmp(a[1], b[1]))
+            y = sorted(y, cmp=lambda a, b: cmp(a[1], b[1]))
             xi, yi = [None]*n, [None]*n
             for sort_i, (file_i, _value) in enumerate(x):
                 xi[file_i] = sort_i
@@ -107,7 +107,7 @@ def parse_mtrack2(fileobj):
     return frames
 
 
-def sinefit(frames, dt = 1.0/30.0):
+def sinefit(frames, dt=1.0/30.0):
     """fit_parameters = sinefit(frames)
     Takes the output of parse_csv and runs a sine-fitting function against it.
     For frames with n dots, returns an n-element list of tuples of SineFit objects (x,y).
@@ -139,9 +139,10 @@ def sinefit(frames, dt = 1.0/30.0):
             raise "Problem with optimize for dot %d in y" % idot
         yfit = SineFit(*py)
         yfit.r2 = stats.mstats.pearsonr(dy, yfit.eval(t))[0] ** 2
-        fit_parameters.append( (xfit, yfit) )
+        fit_parameters.append((xfit, yfit))
 
     return fit_parameters
+
 
 def process_coordinates(fit_parameters):
     """(center_x, center_y, resting_x, resting_y, extended_x, extended_y) = process_coordinates(fit_parameters)
@@ -149,31 +150,26 @@ def process_coordinates(fit_parameters):
     # start by finding a coordinate system based on the center of the device.
     # assume the outer dots make a perfect square and (0,0) is upper left.
     X, Y = 0, 1
-    center_x = ((fit_parameters[-1][0].offset-fit_parameters[-1][0].amplitude) - (fit_parameters[0][0].offset+fit_parameters[0][0].amplitude))/2+fit_parameters[0][0].offset
-    center_y = ((fit_parameters[-1][1].offset-fit_parameters[-1][1].amplitude) - (fit_parameters[0][1].offset+fit_parameters[0][1].amplitude))+fit_parameters[0][1].offset
-    #displacement_sign_x = [sign(dot[X].offset-center_x) for dot in fit_parameters] # negative left of center, positive right of center
-    #displacement_sign_y = [sign(dot[Y].offset-center_y) for dot in fit_parameters] # negative above center, positive below center
+    center_x = ((fit_parameters[-1][0].offset - fit_parameters[-1][0].amplitude) -
+                (fit_parameters[0][0].offset + fit_parameters[0][0].amplitude)) / 2 + fit_parameters[0][0].offset
+    center_y = ((fit_parameters[-1][1].offset - fit_parameters[-1][1].amplitude) -
+                (fit_parameters[0][1].offset + fit_parameters[0][1].amplitude)) + fit_parameters[0][1].offset
 
-    # resting positions fall when the x coordinate is closest to the center
-    # we want: resting posistions to fall when the x coordinate is furthest from center
-    #extended_x = [dot[X].offset - displacement_sign_x[di]*dot[X].amplitude for (di, dot) in enumerate(fit_parameters)]
-    #extended_y =  [yfit.eval(xfit.period/(2*pi) * ((1-displacement_sign_x[di])*pi/4.0 - xfit.phase)) for (di, (xfit, yfit)) in enumerate(fit_parameters)]
-
+    # resting positions fall when y is minimized
     resting_y = [dot[Y].offset + dot[Y].amplitude for dot in fit_parameters]
     resting_x = [xfit.eval(yfit.period/(2*pi) * (pi/2 - yfit.phase)) for (xfit, yfit) in fit_parameters]
 
-    # extended positions fall when the x coordinate is furthest from the center
-    # we want: extended positions to fell when the
+    # extended positions fall where y is maximized
     extended_y = [dot[Y].offset - dot[Y].amplitude for dot in fit_parameters]
     extended_x = [xfit.eval(yfit.period/(2*pi) * (3*pi/2 - yfit.phase)) for (xfit, yfit) in fit_parameters]
-    # resting_x = [xfit.eval(2*pi*yfit.phase / yfit.period) for (xfit, yfit) in fit_parameters]
 
-    #resting_x = [dot[X].offset + displacement_sign_x[di]*dot[X].amplitude for (di, dot) in enumerate(fit_parameters)]
-    # resting_y = [yfit.eval(xfit.period/(2*pi) * ((1+displacement_sign_x[di])*pi/4.0 - xfit.phase)) for (di, (xfit, yfit)) in enumerate(fit_parameters)]
     return (center_x, center_y, resting_x, resting_y, extended_x, extended_y)
 
+
 def find_center_by_frame(frames):
-    return [(np.mean([dot.xpos for dot in frame]), np.mean([dot.ypos for dot in frame])) for frame in frames]
+    return [(np.mean([dot.xpos for dot in frame]),
+             np.mean([dot.ypos for dot in frame])) for frame in frames]
+
 
 def recenter(frames):
     """frames, [[residuals_x, residuals_y]] = recenter(frames)
@@ -198,6 +194,7 @@ def recenter(frames):
             dot.ypos -= centers_y[i]
     return frames, [centers_x, centers_y]
 
+
 def calculate_peak_strain(fit_parameters, resting_x, resting_y, extended_x, extended_y):
     # calculate strain at peak extension
     n_dots = len(fit_parameters)
@@ -208,20 +205,27 @@ def calculate_peak_strain(fit_parameters, resting_x, resting_y, extended_x, exte
     n = int(sqrt(n_dots))
     for di in xrange(n_dots):
         # in x
-        if di % n == 0: # left edge
-            strain_x.append( (extended_x[di+1] - extended_x[di] - (resting_x[di+1] - resting_x[di]) ) / (resting_x[di+1] - resting_x[di]) )
-        elif di % n == (n-1): # right edge
-            strain_x.append( (extended_x[di] - extended_x[di-1] - (resting_x[di] - resting_x[di-1]) ) / (resting_x[di] - resting_x[di-1]) )
-        else: # in the center
-            strain_x.append( (extended_x[di+1] - extended_x[di-1] - (resting_x[di+1] - resting_x[di-1]) ) / (resting_x[di+1] - resting_x[di-1]) )
+        if di % n == 0:  # left edge
+            strain_x.append((extended_x[di+1]-extended_x[di] - (resting_x[di+1]-resting_x[di])) /
+                            (resting_x[di+1]-resting_x[di]))
+        elif di % n == (n-1):  # right edge
+            strain_x.append((extended_x[di]-extended_x[di-1] - (resting_x[di]-resting_x[di-1])) /
+                            (resting_x[di]-resting_x[di-1]))
+        else:  # in the center
+            strain_x.append((extended_x[di+1]-extended_x[di-1] - (resting_x[di+1]-resting_x[di-1])) /
+                            (resting_x[di+1]-resting_x[di-1]))
         # in y
-        if di < n: # top row
-            strain_y.append( (extended_y[di+n] - extended_y[di] - (resting_y[di+n] - resting_y[di]) ) / (resting_y[di+n] - resting_y[di]) )
-        elif di >= n_dots-n: # bottom row
-            strain_y.append( (extended_y[di] - extended_y[di-n] - (resting_y[di] - resting_y[di-n]) )  / (resting_y[di] - resting_y[di-n]) )
-        else: # in the center
-            strain_y.append( (extended_y[di+n] - extended_y[di-n] - (resting_y[di+n] - resting_y[di-n]) ) / (resting_y[di+n] - resting_y[di-n]) )
+        if di < n:  # top row
+            strain_y.append((extended_y[di+n]-extended_y[di] - (resting_y[di+n]-resting_y[di])) /
+                            (resting_y[di+n]-resting_y[di]))
+        elif di >= n_dots-n:  # bottom row
+            strain_y.append((extended_y[di]-extended_y[di-n] - (resting_y[di]-resting_y[di-n])) /
+                            (resting_y[di]-resting_y[di-n]))
+        else:  # in the center
+            strain_y.append((extended_y[di+n]-extended_y[di-n] - (resting_y[di+n]-resting_y[di-n])) /
+                            (resting_y[di+n]-resting_y[di-n]))
     return (strain_x, strain_y)
+
 
 def write_plots(frames, fit_parameters, jitter, directory, min_strain=-0.1, max_strain=0.3, dt=1/30.0):
     # draw residual plots for each sine fit in x and y
@@ -276,16 +280,18 @@ def write_plots(frames, fit_parameters, jitter, directory, min_strain=-0.1, max_
     peak_strain = calculate_peak_strain(fit_parameters, resting_x, resting_y, extended_x, extended_y)
     min_strain = min_strain or min(peak_strain[0] + peak_strain[1])
     max_strain = max_strain or max(peak_strain[0] + peak_strain[1])
-    matrix = lambda axis: np.array(peak_strain[axis]).reshape(n,n)
+    matrix = lambda axis: np.array(peak_strain[axis]).reshape(n, n)
     for (axis, label) in [(0, 'x'), (1, 'y')]:
         plt.clf()
         plt.pcolor(matrix(axis), edgecolor='k', vmin=min_strain, vmax=max_strain)
         for i in range(n):
             for j in range(n):
-                plt.text(i+0.5,j+0.5,"%.4f" % matrix(axis)[j,i],horizontalalignment='center',verticalalignment='center')
+                plt.text(i+0.5, j+0.5, "%.4f" % matrix(axis)[j, i],
+                         horizontalalignment='center',
+                         verticalalignment='center')
         ax = plt.gca()
         ax.set_ylim(ax.get_ylim()[::-1])
-        plt.colorbar(ticks=[-.05,0,.05,.1,.15,.2,.25])
+        plt.colorbar(ticks=[-.05, 0, .05, .1, .15, .2, .25])
         plt.savefig('%s/peakstrain_%s.png' % (directory, label))
 
     f = open('%s/index.html' % directory, 'w')
@@ -327,6 +333,7 @@ def main():
     centered_frames, jitter = recenter(frames)
     fit_parameters = sinefit(frames)
     write_plots(frames, fit_parameters, jitter, directory=args.outpath, min_strain=-0.05, max_strain=0.25)
+
 
 if __name__ == '__main__':
     main()
